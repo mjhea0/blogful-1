c3788dfe4b9434268be3af57db05107d50c56bad
kylelussier <kylelussier3@gmail.com>
2015-01-28 01:19:33 +0000

    updated

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@ import os
 from flask.ext.script import Manager
 
 from blog import app
+from blog.models import Post
+from blog.database import session
 
 manager = Manager(app)
 
@@ -9,6 +11,19 @@ manager = Manager(app)
 def run():
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
+
+
+@manager.command
+def seed():
+    content = """Lorem ipsum dolor sit amet, consectuetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+    
+    for i in range(25):
+        post = Post(
+            title="Test Post #{}".format(i),
+            content=content
+        )
+        session.add(post)
+    session.commit()
     
 if __name__ == "__main__":
     manager.run()
