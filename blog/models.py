465412dd4c2b8285508ab45d61ab8d96ef83e850
kylelussier <kylelussier3@gmail.com>
2015-02-01 17:18:02 +0000

    adding login

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from sqlalchemy import Column, Integer, String, Sequence, Text, DateTime
 
 from database import Base, engine
 
+from flask.ext.login import UserMixin
+
 class Post(Base):
     __tablename__ = "posts"
     
@@ -11,5 +13,14 @@ class Post(Base):
     title = Column(String(1024))
     content = Column(Text)
     datetime = Column(DateTime, default=datetime.datetime.now)
+
+
+class User(Base, UserMixin):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    email = Column(String(128), unique=True)
+    password = Column(String(128))
 
 Base.metadata.create_all(engine)
