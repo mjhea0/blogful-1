a9740fbcd506d7da61f727eb664f8ba66edc1e57
Michael Herman <hermanmu@gmail.com>
2015-02-10 22:44:23 -0700

    updated code

--- blog/config.py
+++ blog/config.py
@@ -5,9 +5,9 @@ class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful"
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "")
-    
+
 
 class TestingConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful-test"
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful_test"
     DEBUG = True
     SECRET_KEY = "Not Secret"
